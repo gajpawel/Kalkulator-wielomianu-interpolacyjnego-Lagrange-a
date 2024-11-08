4cb6aab7fbcf51e0fb6ddd65d33b19048a9476bf
gajpawel <pg305038@student.polsl.pl>
2024-11-08 23:00:05 +0100

    Dodaj pliki projektów.

---
+++ JAAsm/JAAsm.asm
@@ -0,0 +1,7 @@
+.code
+LagrangeAsm proc
+add RCX, RDX
+mov RAX, RCX
+ret
+LagrangeAsm endp
+end
---
+++ MainWindow.xaml.cs
@@ -0,0 +1,133 @@
+﻿using System.Runtime.InteropServices;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Lagrange
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        int parameters = 2;
+        bool asm = true;
+        [DllImport(@"C:\Users\Paweł\Documents\Projekty Visual Studio\Lagrange\x64\Debug\JAAsm.dll")]
+        static extern int LagrangeAsm(int a, int b);
+        public MainWindow()
+        {
+            this.InitializeComponent();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            int a = 2;
+            int b = 3;
+            int c = 0;
+            if(this.asm)
+                c = LagrangeAsm(a, b);
+            result.Text = c.ToString();
+        }
+
+        private void Slider_ValueChanged(object sender, RoutedPropertyChangedEventArgs<double> e)
+        {
+            thr.Text = "Liczba wątków: " + suwakWatki.Value.ToString();
+        }
+
+        private void RadioButton_Checked_Asm(object sender, RoutedEventArgs e)
+        {
+            this.asm = true;
+        }
+
+        private void RadioButton_Checked_Cs(object sender, RoutedEventArgs e)
+        {
+            this.asm = false;
+        }
+
+        private void ComboBox_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            if (variables.SelectedItem is ComboBoxItem selectedItem && selectedItem.Content != null)
+            {
+                if (selectedItem.Content.ToString()=="3")
+                {
+                    x3.Visibility = Visibility.Visible;
+                    y3.Visibility=Visibility.Visible;
+                    x4.Visibility = Visibility.Hidden;
+                    y4.Visibility = Visibility.Hidden;
+                    x5.Visibility = Visibility.Hidden;
+                    y5.Visibility = Visibility.Hidden;
+
+                    Lx3.Visibility = Visibility.Visible;
+                    Ly3.Visibility = Visibility.Visible;
+                    Lx4.Visibility = Visibility.Hidden;
+                    Ly4.Visibility = Visibility.Hidden;
+                    Lx5.Visibility = Visibility.Hidden;
+                    Ly5.Visibility = Visibility.Hidden;
+
+                    this.parameters = 3;
+                }
+                else if(selectedItem.Content.ToString() == "4")
+                {
+                    x3.Visibility = Visibility.Visible;
+                    y3.Visibility = Visibility.Visible;
+                    x4.Visibility = Visibility.Visible;
+                    y4.Visibility = Visibility.Visible;
+                    x5.Visibility = Visibility.Hidden;
+                    y5.Visibility = Visibility.Hidden;
+
+                    Lx3.Visibility = Visibility.Visible;
+                    Ly3.Visibility = Visibility.Visible;
+                    Lx4.Visibility = Visibility.Visible;
+                    Ly4.Visibility = Visibility.Visible;
+                    Lx5.Visibility = Visibility.Hidden;
+                    Ly5.Visibility = Visibility.Hidden;
+
+                    this.parameters = 4;
+                }
+                else if (selectedItem.Content.ToString() == "5")
+                {
+                    x3.Visibility = Visibility.Visible;
+                    y3.Visibility = Visibility.Visible;
+                    x4.Visibility = Visibility.Visible;
+                    y4.Visibility = Visibility.Visible;
+                    x5.Visibility = Visibility.Visible;
+                    y5.Visibility = Visibility.Visible;
+
+                    Lx3.Visibility = Visibility.Visible;
+                    Ly3.Visibility = Visibility.Visible;
+                    Lx4.Visibility = Visibility.Visible;
+                    Ly4.Visibility = Visibility.Visible;
+                    Lx5.Visibility = Visibility.Visible;
+                    Ly5.Visibility = Visibility.Visible;
+
+                    this.parameters = 5;
+                }
+                else
+                {
+                    x3.Visibility = Visibility.Hidden;
+                    y3.Visibility = Visibility.Hidden;
+                    x4.Visibility = Visibility.Hidden;
+                    y4.Visibility = Visibility.Hidden;
+                    x5.Visibility = Visibility.Hidden;
+                    y5.Visibility = Visibility.Hidden;
+
+                    Lx3.Visibility = Visibility.Hidden;
+                    Ly3.Visibility = Visibility.Hidden;
+                    Lx4.Visibility = Visibility.Hidden;
+                    Ly4.Visibility = Visibility.Hidden;
+                    Lx5.Visibility = Visibility.Hidden;
+                    Ly5.Visibility = Visibility.Hidden;
+
+                    this.parameters = 2;
+                }
+            }
+        }
+    }
+}
