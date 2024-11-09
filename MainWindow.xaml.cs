adb4fbc77b20a3fd58da0d6e0c03b5447cc6b672
gajpawel <pg305038@student.polsl.pl>
2024-11-09 13:15:17 +0100

    wczytywanie zmiennych

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -18,7 +18,11 @@ namespace Lagrange
     public partial class MainWindow : Window
     {
         int parameters = 2;
+        List<double> x = new List<double>();
+        List<double> y = new List<double>();
+        int threads;
         bool asm = true;
+
         [DllImport(@"C:\Users\Paweł\Documents\Projekty Visual Studio\Lagrange\x64\Debug\JAAsm.dll")]
         static extern int LagrangeAsm(int a, int b);
         public MainWindow()
@@ -28,6 +32,20 @@ namespace Lagrange
 
         private void Button_Click(object sender, RoutedEventArgs e)
         {
+            List<System.Windows.Controls.TextBox> tx = new List<System.Windows.Controls.TextBox> {x1, x2, x3, x4, x5 };
+            List<System.Windows.Controls.TextBox> ty = new List<System.Windows.Controls.TextBox> { y1, y2, y3, y4, y5 };
+            
+            for (int i = 0; i<this.parameters; ++i)
+            {
+                if (tx[i].Text.Length == 0 || ty[i].Text.Length == 0)
+                { 
+                    this.x.Clear();
+                    this.y.Clear();
+                    return; 
+                }
+                this.x.Add(double.Parse(tx[i].Text));
+                this.y.Add(double.Parse(ty[i].Text));
+            }
             int a = 2;
             int b = 3;
             int c = 0;
@@ -39,6 +57,7 @@ namespace Lagrange
         private void Slider_ValueChanged(object sender, RoutedPropertyChangedEventArgs<double> e)
         {
             thr.Text = "Liczba wątków: " + suwakWatki.Value.ToString();
+            this.threads = int.Parse(suwakWatki.Value.ToString());
         }
 
         private void RadioButton_Checked_Asm(object sender, RoutedEventArgs e)
